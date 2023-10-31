087b2f2eb39908dc01716171156bd21adb256722
yola_liang <100056224+yol4L@users.noreply.github.com>
2023-10-31 22:44:35 +1100

    Implement nav tabs UI designs

--- app/components/Tabs/navTabs.tsx
+++ app/components/Tabs/navTabs.tsx
@@ -1,20 +1,26 @@
 "use client";
 
+const NAV_ITEMS: { id: string; name: string; path: string }[] = [
+  { id: "home", name: "home", path: "/" },
+  { id: "profile", name: "profile", path: "/portfolio" },
+  { id: "recent_work", name: "work", path: "/recent-work" },
+  { id: "blog", name: "blog", path: "/blog" },
+  { id: "contact", name: "contact", path: "/contact" },
+];
+
 export default function NavTabs() {
   return (
-    <div className="flex items-center gap-10">
-      <span>
-        <a href="/portfolio">PORTFOLIO</a>
-      </span>
-      <span>
-        <a href="/recent-work">WORK</a>
-      </span>
-      <span>
-        <a href="/blog">BLOG</a>
-      </span>
-      <span>
-        <a href="/contact">CONTACT</a>
-      </span>
+    <div className="flex items-center gap-8">
+      {NAV_ITEMS.map(({ id, name, path }) => {
+        return (
+          <span
+            key={id}
+            className="tracking-wider text-neutral-800 hover:text-cerulean-500 hover:border-b hover:border-cerulean-500"
+          >
+            <a href={path}>{name.toUpperCase()}</a>
+          </span>
+        );
+      })}
     </div>
   );
 }
