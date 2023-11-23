cf1735ab040e9c924b9008b3544b6285c406a047
yola_liang <100056224+yol4L@users.noreply.github.com>
2023-11-23 23:24:50 +1100

    Validate contact form fields

--- app/(core)/contact/contact.tsx
+++ app/(core)/contact/contact.tsx
@@ -1,6 +1,183 @@
+"use client";
+
 import Link from "next/link";
 import cx from "classnames";
 import { ELSE_ITEMS, PERSONAL_INFO_ITEMS } from "@/app/data";
+import { useEffect, useState } from "react";
+
+const ContactForm = ({
+  inView,
+  scrollingUp,
+}: {
+  inView: boolean;
+  scrollingUp: boolean;
+}) => {
+  // States for form values and validity
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [formValidity, setFormValidity] = useState({
+    name: false,
+    email: false,
+    message: false,
+  });
+  const [attemptedSubmit, setAttemptedSubmit] = useState(false);
+
+  // Email validation regex pattern
+  const emailRegex =
+    /^[A-Za-z0-9]+([_\-\.][A-Za-z0-9]+)*@([A-Za-z0-9]+([\-]*[A-Za-z0-9]+)*\.)+[A-Za-z]{2,6}$/;
+
+  // Validates a single field
+  const validateField = (fieldName: string, value: string) => {
+    let isValid = value.trim() !== "";
+    if (fieldName === "email") {
+      isValid = emailRegex.test(value);
+    }
+    setFormValidity((prev) => ({ ...prev, [fieldName]: isValid }));
+  };
+
+  // Updates form values on input change
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormValues((prev) => ({ ...prev, [name]: value }));
+    validateField(name, value);
+  };
+
+  // Checks if all fields in the form are valid
+  const validateForm = () => {
+    return Object.values(formValidity).every(Boolean);
+  };
+
+  // Handles form submission
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setAttemptedSubmit(true); // Indicates that a submit attempt was made
+
+    // Validate all fields before submission
+    Object.keys(formValues).forEach((key) => {
+      validateField(key, formValues[key as keyof typeof formValues]);
+    });
+
+    if (!validateForm()) {
+      // If any field is invalid, prevent form submission
+      return;
+    }
+
+    // Process form submission if all fields are valid
+    console.log("Submitted:", formValues);
+
+    // Reset form fields and validity state after successful submission
+    setFormValues({ name: "", email: "", message: "" });
+    setFormValidity({ name: false, email: false, message: false });
+    setAttemptedSubmit(false); // Reset the submission attempt state
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={cx(
+        "w-1/2 py-2 flex flex-col gap-8",
+        inView && !scrollingUp && "animate-slideLeft"
+      )}
+    >
+      {/* Name */}
+      <div className="flex flex-col">
+        <label htmlFor="name" className="font-medium text-ocean-500 mb-2">
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={formValues.name}
+          onChange={handleInputChange}
+          className={cx(
+            "p-2 bg-transparent rounded-md border text-slate-800 dark:text-slate-100 transition-colors duration-700",
+            formValidity.name || !attemptedSubmit
+              ? "border-slate-200 dark:border-slate-500 focus:border-ocean-500 focus:ring-ocean-500"
+              : "border-red-400 focus:border-red-400 focus:ring-red-400 dark:border-red-300 dark:focus:ring-red-300"
+          )}
+        />
+        <div className="h-1">
+          {!formValidity.name && attemptedSubmit && (
+            <p className="text-xs mt-1 text-red-400 dark:text-red-300 transition-colors duration-700">
+              ! Your name is required.
+            </p>
+          )}
+        </div>
+      </div>
+      {/* Email */}
+      <div className="flex flex-col">
+        <label htmlFor="email" className="font-medium text-ocean-500 mb-2">
+          Email Address
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={formValues.email}
+          onChange={handleInputChange}
+          className={cx(
+            "p-2 bg-transparent rounded-md border text-slate-800 dark:text-slate-100 transition-colors duration-700",
+            formValidity.email || !attemptedSubmit
+              ? "border-slate-200 dark:border-slate-500 focus:border-ocean-500 focus:ring-ocean-500"
+              : "border-red-400 focus:border-red-400 focus:ring-red-400 dark:border-red-300 dark:focus:ring-red-300"
+          )}
+        />
+        <div className="h-1">
+          {!formValidity.email && attemptedSubmit && (
+            <p className="text-xs mt-1 text-red-400 dark:text-red-300 transition-colors duration-700">
+              ! Please enter a valid email address.
+            </p>
+          )}
+        </div>
+      </div>
+      {/* Message */}
+      <div className="flex flex-col">
+        <label htmlFor="message" className="font-medium text-ocean-500 mb-2">
+          Message
+        </label>
+        <textarea
+          name="message"
+          id="message"
+          cols={30}
+          rows={10}
+          value={formValues.message}
+          onChange={handleInputChange}
+          className={cx(
+            "p-2 bg-transparent rounded-md border text-slate-800 dark:text-slate-100 transition-colors duration-700",
+            " ",
+            formValidity.message || !attemptedSubmit
+              ? "border-slate-200 dark:border-slate-500 focus:border-ocean-500 focus:ring-ocean-500"
+              : "border-red-400 focus:border-red-400 focus:ring-red-400 dark:border-red-300 dark:focus:ring-red-300"
+          )}
+        ></textarea>
+        <div className="h-1">
+          {!formValidity.message && attemptedSubmit && (
+            <p className="text-xs mt-1 text-red-400 dark:text-red-300 transition-colors duration-700">
+              ! Please type your message here.
+            </p>
+          )}
+        </div>
+      </div>
+      <button
+        type="submit"
+        className={cx(
+          "w-auto px-8 py-4 self-start rounded-full font-medium tracking-widest transition-colors duration-700",
+          formValidity.name && formValidity.email && formValidity.message
+            ? "bg-slate-300 dark:bg-ocean-500 text-ocean-500 dark:text-slate-100"
+            : "bg-gray-300 text-gray-500 "
+        )}
+      >
+        <span>SUBMIT MESSAGE</span>
+      </button>
+    </form>
+  );
+};
 
 export default function ContactMeView({
   inView,
@@ -80,72 +257,7 @@ export default function ContactMeView({
         </div>
       </div>
       {/* Message form */}
-      <form
-        className={cx(
-          "w-1/2 py-2 flex flex-col gap-8",
-          inView && !scrollingUp && "animate-slideLeft"
-        )}
-      >
-        {/* Name */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="name" className="font-medium text-ocean-500">
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className={cx(
-              "p-2 bg-transparent rounded-md border transition-colors duration-700",
-              "border-slate-200 dark:border-slate-500",
-              "focus:outline-ocean-500"
-            )}
-          />
-        </div>
-        {/* Email */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="email" className="font-medium text-ocean-500">
-            Email Address
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className={cx(
-              "p-2 bg-transparent rounded-md border transition-colors duration-700",
-              "border-slate-200 dark:border-slate-500",
-              "focus:outline-ocean-500"
-            )}
-          />
-        </div>
-        {/* Message */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="message" className="font-medium text-ocean-500">
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            cols={30}
-            rows={10}
-            className={cx(
-              "p-2 bg-transparent rounded-md border transition-colors duration-700",
-              "border-slate-200 dark:border-slate-500",
-              "focus:outline-ocean-500"
-            )}
-          ></textarea>
-        </div>
-        <button
-          type="button"
-          className={cx(
-            "w-auto px-8 py-4 self-start rounded-full font-medium tracking-widest transition-colors duration-700",
-            "bg-slate-300 dark:bg-ocean-500",
-            "text-ocean-500 dark:text-slate-100"
-          )}
-        >
-          <span>SUBMIT MESSAGE</span>
-        </button>
-      </form>
+      <ContactForm inView={inView} scrollingUp={scrollingUp} />
     </div>
   );
 }
