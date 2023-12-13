5a9dfc702cb49a05cadb73a0501f953cc5e541e9
yola_liang <100056224+yol4L@users.noreply.github.com>
2023-12-13 17:45:18 +1100

    Add og tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,23 @@ export const metadata: Metadata = {
   title: "Yola Liang - Web Developer & Software Engineer",
   description:
     "Discover Yola Liang's innovative web and software engineering work. Expert in full-stack development, app creation, and modern tech solutions.",
+  openGraph: {
+    title: "Yola Liang - Web Developer & Software Engineer",
+    description:
+      "Discover Yola Liang's innovative web and software engineering work.",
+    url: "",
+    siteName: "Yola's digital world",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  metadataBase: new URL("https://yolaliang.me"),
 };
 
 export default function RootLayout({
